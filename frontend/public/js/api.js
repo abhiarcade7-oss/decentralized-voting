8ba3e59c5243b3b6c38b94f25a3c096ed0e5fe7c
abhiarcade7-oss <abhiarcade7@gmail.com>
2026-02-10 22:35:39 +0530

    Update api.js

--- frontend/public/js/api.js
+++ frontend/public/js/api.js
@@ -1,5 +1,7 @@
 // frontend/public/js/api.js
-export const API_BASE = "http://localhost:5000";
+// export const API_BASE = "http://localhost:5000";
+export const API_BASE = "https://voting-backend.onrender.com";
+
 
 export async function apiFetch(path, options = {}) {
     const res = await fetch(API_BASE + path, options);
